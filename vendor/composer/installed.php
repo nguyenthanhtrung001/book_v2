d5a982b07e4b4047f9162455fb2f25e4619e2f73
nguyenthanhtrung001 <thanhtrung8674@gmail.com>
2024-06-20 18:19:20 +0700

    update test

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hackdest/wordpress-cicd',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '602e8d3dcf8f512e76255895f4652ad0ee30faa9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hackdest/wordpress-cicd' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '602e8d3dcf8f512e76255895f4652ad0ee30faa9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
